3248d252dd7a7aa6bda4f073524ad183093df215
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-04-16 02:35:39 +0300

    commit tree 63512494e9cd251aa7d3cec6e2e82bd01546beeb

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+
+
+function writeCards(name,event){
+    let messages = []
+    
+
+    for(let i = 0;i<name.length;i++)
+    {
+        let outputMsg = `Thank you, ${name[i]}, for the wonderful ${event} gift!`
+        messages.push(outputMsg)
+
+    
+        
+
+    }
+    return messages
+
+}
+
+function countDown(num)
+{
+    let count = num;
+    while(count>=0)
+    {
+        console.log(count);
+        count--
+    }
+}
+
+
+   
+
